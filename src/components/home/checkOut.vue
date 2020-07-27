6536b297cbac372c4ce0de6a27ec7ae71bdb2292
郑竹兵 <zhubing.zheng@fortrun.cn>
2020-07-27 14:47:45 +0800

    '退款'

--- src/components/home/checkOut.vue
+++ src/components/home/checkOut.vue
@@ -71,7 +71,7 @@
                   <span class="red">{{orderDetail.refundVO.refundFeeStr}}元</span>
                 </div>
                 <div class="list">
-                  <el-button type="primary" :loading="false" class="btn green"  @click="payInfoClick(orderDetail.deposits[0])" v-if="!orderDetail.deposits[0].refund && !orderDetail.isFreeDeposit">退款</el-button>
+                  <el-button type="primary" :loading="false" class="btn green"  @click="payInfoClick(orderDetail.deposits[0], 1)" v-if="!orderDetail.deposits[0].refund && !orderDetail.isFreeDeposit">退款</el-button>
                 </div>
               </div>
             </div>
